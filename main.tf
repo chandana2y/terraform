4d5c640ea84674a165ac97d360895fc25c6218d1
chandana2y <96794536+chandana2y@users.noreply.github.com>
2021-12-29 22:11:35 -0800

    web app

--- main.tf
+++ main.tf
@@ -1,24 +1,37 @@
-# valid for terraform version 0.13
-# provider "azurerm" {
-#   version = "=2.40.0"
-#   features {}
-# }
-
-# valid for terraform version >= 0.14
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.40.0"
-    }
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myFirstResourceGroup"
-  location = "westeurope"
+resource "azurerm_app_service" "example" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
 }
